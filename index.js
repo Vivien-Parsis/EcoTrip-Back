320ff1b023519b9a78b20aa9e6411bf51d46f18d
Vivien-Parsis <vivienparsis.perso@gmail.com>
2024-03-19 14:10:51 +0100

    changed to express from fastify

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
+//import
+const express = require("express")
+const app = express()
+const carFleetRouter = require("./src/router/carFleetRoute")
+//const
 const host = ("RENDER" in process.env) ? `0.0.0.0` : `localhost`
 const port = process.env.PORT || 3000
-const mongoUrl = 'mongodb://localhost:27017'
-const fastify = require("fastify")({logger:true})
+//plugin
+app.use('/api/carfleet', carFleetRouter)
 
-fastify.register(require('@fastify/mongodb'), {
-    forceClose: true,
-    url: mongoUrl
-  })
-fastify.get("/",(req, res)=>{
-    res.send("Hello World !")
-})
-
-fastify.register(require('./src/router/carFleetRoute'), { prefix: '/api/carfleet' })
-fastify.register(require('./src/router/userListRoute'), { prefix: '/api/userlist' })
-
-fastify.listen({host: host, port: port }, (err, address) => {
-    if (err) throw err
-    console.log(`listening to ${address}`)
+app.listen(port, ()=>{
+    console.log(`Le serveur API écoute sur http://${host}:${port}`);
 })
--- src/router/carFleetRoute.js
+++ src/router/carFleetRoute.js
@@ -1,17 +1,7 @@
-const { ObjectId } = require("@fastify/mongodb")
-const db = require("../middleware/db")
-const collName = "CarFleet"
+const router = require('express').Router();
 
-module.exports = async function (fastify, opts, done) {
-    fastify.get('/get/:id', async function (req, res) {
-        const carFleet = fastify.mongo.client.db(db.DBName).collection(collName)
-        try{
-            const data = await carFleet.find(req.params.id ? {_id : new ObjectId(req.params.id)} : {})
-            res.send(await data.toArray())
-        }catch(err){
-            if (err) throw err;
-            res.send("err")
-        }
-    })
-    done()
-}
+router.get("/get/:id?", (req, res) => {
+    res.send("/api/carfleet/get")
+})
+
+module.exports = router
