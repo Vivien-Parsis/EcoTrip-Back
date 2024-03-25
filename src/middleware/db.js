b91875d8dc629b37dbbd9626447ff044bceb0fb4
Vivien-Parsis <94974717+Vivien-Parsis@users.noreply.github.com>
2024-03-25 13:14:08 +0100

    Update db.js

--- src/middleware/db.js
+++ src/middleware/db.js
@@ -1,33 +1,34 @@
 const { default: mongoose } = require("mongoose");
 const { db_url } = require("./../const/config")
-const car = null
-const user = null
-const trip = null
 
-const initDB = async () => {
-        await mongoose.connect(db_url);
-        car = await mongoose.model('cars', {
-                modele: { type: String, required: true },
-                capacite: { type: Number, required: true },
-                energie: { type: String, required: true }})
-        user = await mongoose.model('users',{
-            nom: { type: String, required: true },
-            prenom: { type: String, required: true },
-            email: { type: String, required: true, unique: true },
-            photo: String,
-            vehicules: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Voiture' }],
-            motDePasse: { type: String, required: true },
-            score: { type: Number, default: 0 }})
-        trip = await mongoose.model('trips',{
-            debut: { type: Date, required: true },
-            fin: { type: Date, required: true },
-            distance: { type: Number, required: true },
-            conducteurs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Utilisateur' }],
-            lieuDepart: String,
-            lieuFin: String,
-            passagers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Utilisateur' }],
-        })
-}
-initDB()  
+mongoose.connect(db_url)
+        .then(() => console.log('Connecté avec succès à MongoDB'))
+        .catch((err) => console.error('Erreur lors de la connexion à MongoDB :', err));
+
+const car = mongoose.model('cars', {
+    modele: { type: String, required: true },
+    capacite: { type: Number, required: true },
+    energie: { type: String, required: true },
+})
+
+const user = mongoose.model('users',{
+    nom: { type: String, required: true },
+    prenom: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    photo: String,
+    vehicules: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Voiture' }],
+    motDePasse: { type: String, required: true },
+    score: { type: Number, default: 0 },
+})
+
+const trip = mongoose.model('trips',{
+    debut: { type: Date, required: true },
+    fin: { type: Date, required: true },
+    distance: { type: Number, required: true },
+    conducteurs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Utilisateur' }],
+    lieuDepart: String,
+    lieuFin: String,
+    passagers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Utilisateur' }],
+})
 
 module.exports = { car, user, trip }
