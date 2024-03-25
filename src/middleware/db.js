b3eaca302e574d9e48922fcf0cf1df043eaeb8d7
Vivien-Parsis <94974717+Vivien-Parsis@users.noreply.github.com>
2024-03-25 13:03:22 +0100

    Update db.js

--- src/middleware/db.js
+++ src/middleware/db.js
@@ -5,13 +5,12 @@ const user = null
 const trip = null
 
 const initDB = async () => {
-        const connection = mongoose.createConnection()
         await mongoose.connect(db_url);
-        await car = mongoose.model('cars', {
+        car = await mongoose.model('cars', {
                 modele: { type: String, required: true },
                 capacite: { type: Number, required: true },
                 energie: { type: String, required: true }})
-        await user = mongoose.model('users',{
+        user = await mongoose.model('users',{
             nom: { type: String, required: true },
             prenom: { type: String, required: true },
             email: { type: String, required: true, unique: true },
@@ -19,7 +18,7 @@ const initDB = async () => {
             vehicules: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Voiture' }],
             motDePasse: { type: String, required: true },
             score: { type: Number, default: 0 }})
-        await trip = mongoose.model('trips',{
+        trip = await mongoose.model('trips',{
             debut: { type: Date, required: true },
             fin: { type: Date, required: true },
             distance: { type: Number, required: true },
