57c418796d04f81e3bf4ef54d906c84b82872e19
Vivien-Parsis <vivienparsis.perso@gmail.com>
2024-04-18 00:45:03 +0200

    fix calculate score

--- src/controller/score.js
+++ src/controller/score.js
@@ -4,6 +4,15 @@ facteurEmision : Emission CO2(kg/km)
 consoLitreParCentKm : consomation litre carburant pour cent km
 */
 const calculateScoreTrajet = ( distance=0, facteurEmision=0, consoLitreParCentKm=0) => {
+    if(!distance){
+        distance = 0
+    } 
+    if(!facteurEmision){
+        facteurEmision = 0
+    }
+    if(!consoLitreParCentKm){
+        consoLitreParCentKm = 0
+    }
     return consoLitreParCentKm*facteurEmision*distance
 } 
 
